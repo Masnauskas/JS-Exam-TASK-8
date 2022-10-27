43158c33eeef6001be21521aa0c5958545b52914
Tadas <tadasmasnauskas@gmail.com>
2022-10-27 09:05:36 +0300

    As per Task left only ES5 class constructor

--- script.js
+++ script.js
@@ -6,28 +6,6 @@ multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
 
-//ES6
-// class Calculator {
-//   constructor(value1, value2) {
-//     this.value1 = value1;
-//     this.value2 = value2;
-//   }
-//   sum() {
-//     console.log(this.value1 + this.value2);
-//   }
-//   subtraction() {
-//     console.log(this.value1 - this.value2);
-//   }
-//   multiplication() {
-//     console.log(this.value1 * this.value2);
-//   }
-//   division() {
-//     console.log(this.value1 / this.value2);
-//   }
-// }
-
-// const calc = new Calculator(5, 5);
-
 //ES5
 function Calculator(value1, value2) {
   this.value1 = value1;
